53bc99707e7dae21c9306b9e17d4b512633e8583
Stefan Jovanovic <stefan.jovanavich@gmail.com>
2021-07-04 16:22:54 +0200

    Create GoogleButton.kt

---
+++ GoogleButton.kt
@@ -0,0 +1,85 @@
+import androidx.compose.animation.animateContentSize
+import androidx.compose.animation.core.LinearOutSlowInEasing
+import androidx.compose.animation.core.tween
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.*
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.Shape
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.example.composetest.ui.theme.Shapes
+
+@ExperimentalMaterialApi
+@Composable
+fun GoogleButton(
+    text: String = "Sign Up with Google",
+    loadingText: String = "Creating Account...",
+    icon: Int = R.drawable.ic_google_logo,
+    shape: Shape = Shapes.medium,
+    borderColor: Color = Color.LightGray,
+    backgroundColor: Color = MaterialTheme.colors.surface,
+    progressIndicatorColor: Color = MaterialTheme.colors.primary,
+    onClicked: () -> Unit
+) {
+    var clicked by remember { mutableStateOf(false) }
+
+    Surface(
+        onClick = { clicked = !clicked },
+        shape = shape,
+        border = BorderStroke(width = 1.dp, color = borderColor),
+        color = backgroundColor
+    ) {
+        Row(
+            modifier = Modifier
+                .padding(
+                    start = 12.dp,
+                    end = 16.dp,
+                    top = 12.dp,
+                    bottom = 12.dp
+                )
+                .animateContentSize(
+                    animationSpec = tween(
+                        durationMillis = 300,
+                        easing = LinearOutSlowInEasing
+                    )
+                ),
+            verticalAlignment = Alignment.CenterVertically,
+            horizontalArrangement = Arrangement.Center
+        ) {
+            Icon(
+                painter = painterResource(id = icon),
+                contentDescription = "Google Button",
+                tint = Color.Unspecified
+            )
+            Spacer(modifier = Modifier.width(8.dp))
+            Text(text = if (clicked) loadingText else text)
+            if (clicked) {
+                Spacer(modifier = Modifier.width(16.dp))
+                CircularProgressIndicator(
+                    modifier = Modifier
+                        .height(16.dp)
+                        .width(16.dp),
+                    strokeWidth = 2.dp,
+                    color = progressIndicatorColor
+                )
+                onClicked()
+            }
+        }
+    }
+}
+
+@ExperimentalMaterialApi
+@Composable
+@Preview
+private fun GoogleButtonPreview() {
+    GoogleButton(
+        text = "Sign Up with Google",
+        loadingText = "Creating Account...",
+        onClicked = {}
+    )
+}
