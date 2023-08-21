c8b24099c0a06e21bd69daa8d8712e7abd7bdfb3
Stefan Jovanovic <stefan.jovanavich@gmail.com>
2023-08-21 20:42:51 +0200

    Update GoogleButton.kt
    Adapted to Material3 and fixed onClicked() lambda.

--- GoogleButton.kt
+++ GoogleButton.kt
@@ -2,17 +2,29 @@ import androidx.compose.animation.animateContentSize
 import androidx.compose.animation.core.LinearOutSlowInEasing
 import androidx.compose.animation.core.tween
 import androidx.compose.foundation.BorderStroke
-import androidx.compose.foundation.layout.*
-import androidx.compose.material.*
-import androidx.compose.runtime.*
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.material3.CircularProgressIndicator
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.Shape
 import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import com.example.composetest.ui.theme.Shapes
 
 @Composable
 fun GoogleButton(
@@ -20,16 +32,18 @@ fun GoogleButton(
     text: String = "Sign Up with Google",
     loadingText: String = "Creating Account...",
     icon: Int = R.drawable.ic_google_logo,
-    shape: Shape = Shapes.medium,
+    shape: Shape = MaterialTheme.shapes.medium,
     borderColor: Color = Color.LightGray,
-    backgroundColor: Color = MaterialTheme.colors.surface,
-    progressIndicatorColor: Color = MaterialTheme.colors.primary,
+    backgroundColor: Color = MaterialTheme.colorScheme.surface,
+    progressIndicatorColor: Color = MaterialTheme.colorScheme.primary,
     onClicked: () -> Unit
 ) {
     var clicked by remember { mutableStateOf(false) }
-
     Surface(
-        modifier = modifier.clickable { clicked = !clicked },
+        modifier = modifier.clickable {
+            clicked = !clicked
+            onClicked()
+        },
         shape = shape,
         border = BorderStroke(width = 1.dp, color = borderColor),
         color = backgroundColor
@@ -67,18 +81,7 @@ fun GoogleButton(
                     strokeWidth = 2.dp,
                     color = progressIndicatorColor
                 )
-                onClicked()
             }
         }
     }
-}
-
-@Composable
-@Preview
-private fun GoogleButtonPreview() {
-    GoogleButton(
-        text = "Sign Up with Google",
-        loadingText = "Creating Account...",
-        onClicked = {}
-    )
 }
